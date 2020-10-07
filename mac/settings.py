38b490784567f87945aced1f2cda28810f15a6c5
Jash Jain <jashjain21@gmail.com>
2020-10-07 13:42:32 +0530

    Security changes

--- mac/settings.py
+++ mac/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bjs@ut*bz)q9zz(zy=971!q#soc4m%sj%x3+4+8gb*!6(!c84p'
+from decouple import config ###
+
+SECRET_KEY = config("SECRET_KEY") # this have to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
